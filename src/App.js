e66e7462d5267f2f525310c765a650076f5f3a20
tosi4ka <61636042+tosia2013@users.noreply.github.com>
2021-11-10 17:28:54 +0300

    Revert "new in navigation"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React, { Component } from "react";
 import Routers from "./components/Router";
 import "./style/app.css";
-import Content from "./components/Content";
 
 class App extends Component {
   render() {
-    return (
-      <>
-        <Routers />
-        <Content />
-      </>
-    );
+    return <Routers />;
   }
 }
 
